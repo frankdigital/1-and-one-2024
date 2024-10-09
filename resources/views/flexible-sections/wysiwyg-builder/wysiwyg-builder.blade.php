8a6e4840c4f87a9aeba24a8c97416f60830e92eb
Sam <sam@frankdigital.com.au>
2024-10-09 17:47:56 +1100

    remove contain from wysiwyg

--- resources/views/flexible-sections/wysiwyg-builder/wysiwyg-builder.blade.php
+++ resources/views/flexible-sections/wysiwyg-builder/wysiwyg-builder.blade.php
@@ -1,15 +1,15 @@
 @php
-    $baseClass = 'wysiwyg-builder';
+	$baseClass = 'wysiwyg-builder';
 @endphp
-<x-section @class([ccn($baseClass)])>
-    <x-container @class([ccn($baseClass, 'container')])>
-        @isset($content['wysiwyg_builder'])
-            @foreach ($content['wysiwyg_builder'] as $item)
-                @includeIf("partials.wysiwyg.{$item['acf_fc_layout']}", [
-                    'section' => $item,
-                    'class' => ccn($baseClass, 'block'),
-                ])
-            @endforeach
-        @endisset
-    </x-container>
+<x-section :contain="false" @class([ccn($baseClass)])>
+	<x-container @class([ccn($baseClass, 'container')])>
+		@isset($content['wysiwyg_builder'])
+			@foreach ($content['wysiwyg_builder'] as $item)
+				@includeIf("partials.wysiwyg.{$item['acf_fc_layout']}", [
+					'section' => $item,
+					'class' => ccn($baseClass, 'block'),
+				])
+			@endforeach
+		@endisset
+	</x-container>
 </x-section>

94131116b3c8d64d48936b0092e32ffc8a3680b1
Sam <sam@frankdigital.com.au>
2024-10-14 14:41:46 +1100

    increase gap in cta contact for hero

--- resources/views/heroes/secondary/standard.blade.php
+++ resources/views/heroes/secondary/standard.blade.php
@@ -36,7 +36,7 @@
 			@if ($showCta)
 				<x-cta-container :fullWidth="true" @class([ccn($baseClass, 'cta-container')])>
 					<x-cta classes="" priority="primary" :cta="$content['primary_cta']" />
-					<x-cta classes="" priority="secondary" :cta="$content['secondary_cta']" />
+					<x-cta classes="" priority="text-link" :cta="$content['secondary_cta']" />
 				</x-cta-container>
 			@endif
 		</div>

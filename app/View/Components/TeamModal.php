3b6535cc37dd0e2f51b1af84e0d4e78f3ba5e378
Sam <sam@frankdigital.com.au>
2024-11-20 14:15:00 +1100

    update team modal to show phone icon if toggled

--- app/View/Components/TeamModal.php
+++ app/View/Components/TeamModal.php
@@ -26,11 +26,18 @@ class TeamModal extends Component {
 	 * @return void
 	 */
 	public function __construct(\WP_Post $post) {
+		$phoneEnabled = $this->getAcfFieldFromID('post_type_data_contact_enable_phone', $post);
+		$phone = null;
+
+		if ($phoneEnabled) {
+			$phone = $this->getAcfFieldFromID('post_type_data_contact_phone_number', $post);
+		}
+
 		$this->content = [
 			'image' => $this->buildImageFromId(get_post_thumbnail_id($post)),
 			'name' => $this->getAcfFieldFromID('post_type_data_name', $post) ?? get_the_title($post),
 			'position' => $this->getAcfFieldFromID('post_type_data_position', $post),
-			'phone' => $this->getAcfFieldFromID('post_type_data_contact_phone_number', $post),
+			'phone' => $phone,
 			'email' => $this->getAcfFieldFromID('post_type_data_contact_email', $post),
 			'linkedin' => $this->getAcfFieldFromID('post_type_data_contact_linkedin', $post),
 			'description' => $this->getAcfFieldFromID('post_type_data_description', $post),

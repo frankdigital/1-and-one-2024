8c670579ba95f31ed15a9098b8a1804ad830a24a
Sam <sam@frankdigital.com.au>
2025-01-03 09:31:58 +1100

    add SEO schema data to pages

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,51 @@
 		    gtm4wp_the_gtm_tag();
 		} ?>
 
+		@if (is_front_page())
+			<script type="application/ld+json">
+				{
+					"@context": "http://schema.org",
+					"@type": "LocalBusiness",
+					"address": "Level 1/18 National Circuit, Barton ACT 2600",
+					"name": "1 and One Consulting",
+					"image": "https://www.1andone.com.au/wp-content/uploads/2024/09/1andOne_Master_Primary_Logo.png",
+					"priceRange": "$",
+					"telephone": "02 6122 1400",
+					"url": "https://www.1andone.com.au/",
+					"openingHoursSpecification": [
+						{
+							"@type": "OpeningHoursSpecification",
+							"dayOfWeek": [
+								"Monday",
+								"Tuesday",
+								"Wednesday",
+								"Thursday",
+								"Friday"
+							],
+							"opens": "09:00",
+							"closes": "17:00"
+						}
+					],
+					"hasMap": "https://maps.app.goo.gl/ST1b9C9mRn892QkPA"
+				}
+			</script>
+		@else
+			<script type="application/ld+json">
+				{
+					"@context": "http://schema.org",
+					"@type": "Organization",
+					"name": "1 and One Consulting",
+					"url": "https://www.1andone.com.au/",
+					"logo": "https://www.1andone.com.au/wp-content/uploads/2024/09/1andOne_Master_Primary_Logo.png",
+					"sameAs": [
+						"https://au.linkedin.com/company/1-and-one",
+						"https://www.finance.gov.au/node/4434",
+						"https://buy.nsw.gov.au/supplier/profile/105189"
+					]
+				}
+			</script>
+		@endif
+
 		@php(wp_body_open())
 
 		<div id="app">
